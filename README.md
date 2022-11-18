359ed44de2a91fe50a5717b03a8a1b96754e32af
LucaFrioli <lucafrioli7@gmail.com>
2022-11-18 16:11:50 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# forum-ciclo-3-e-4
+ Fórum_de_encerramento
---
+++ sistema_fórum_3_4/js/System.js
@@ -0,0 +1,7 @@
+const nome = prompt(`Digite seu Nome:`)
+const Capitans = nome[0].toUperCase() + nome.substring(1)
+document.alert(`${nome} seja bem vindo ao sistema`)
+
+function diga() {
+}
+
