689bf4a0f594fc08846f788f8733b23ff65c0873
LucaFrioli <lucafrioli7@gmail.com>
2022-11-21 17:19:43 -0300

    Inicio dos cálculos

--- sistema_fórum_3_4/js/System.js
+++ sistema_fórum_3_4/js/System.js
@@ -1,7 +1,25 @@
-const nome = prompt(`Digite seu Nome:`)
-const Capitans = nome[0].toUperCase() + nome.substring(1)
-document.alert(`${nome} seja bem vindo ao sistema`)
+const calcular = document.querySelector(`button#bntCalcule`)
 
-function diga() {
+
+function imc() {
+    const nome = document.querySelector(`input#nome`).value
+    const peso = document.querySelector(`input#peso`).value
+    const altura = document.querySelector(`input#altura`).value
+    const result = document.querySelector(`span#resposta`)
+
+    if (nome !== `` && peso !== `` && altura !== ``) {
+        const ImcRes = (peso / (altura ** 2)).toFixed(2)
+
+        switch (ImcRes) {
+            case (ImcRes < 18.5):
+                result.textContent = ImcRes
+                break
+        }
+    } else {
+        result.textContent = `Não esqueçade prencher todos os campos !!!`
+    }
+
 }
 
+
+calcular.addEventListener(`click`, imc)
